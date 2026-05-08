9bcfa359f52eeaac4c1a0d77571530da77491921
Heiligetomate <heiligetomate@protonmail.com>
2026-05-09 00:09:52 +0200

    errors

--- src/consts.rs
+++ src/consts.rs
@@ -3,3 +3,4 @@ pub const ONLY_STRUCTS: &str = "#[derive(ProgressBar)] only supports structs";
 pub const ONLY_NAMED: &str = "#[derive(ProgressBar)] only supports structs with named fields";
 pub const NO_FIELDS: &str = "#[derive(ProgressBar)] requires exactly one field with #[max_value = \"...\"], found none";
 pub const TOO_MANY_FIELDS: &str = "[max_value] used on multiple fields — exactly one allowed";
+pub const PARSE_ERROR: &str = "Can not parse this shit";
--- src/errors.rs
+++ src/errors.rs
@@ -1,28 +1,31 @@
 use proc_macro::TokenStream;
 use syn::{DeriveInput, Error};
 
-use crate::consts::{NO_FIELDS, ONLY_NAMED, ONLY_STRUCTS, TOO_MANY_FIELDS};
+use crate::consts::{NO_FIELDS, ONLY_NAMED, ONLY_STRUCTS, PARSE_ERROR, TOO_MANY_FIELDS};
+
+fn new_spanned_error(input: &DeriveInput, message: &str) -> TokenStream {
+    Error::new_spanned(input, message)
+        .to_compile_error()
+        .into()
+}
 
 pub fn only_named_structs(input: &DeriveInput) -> TokenStream {
-    return Error::new_spanned(&input.ident, ONLY_NAMED)
-        .to_compile_error()
-        .into();
+    new_spanned_error(input, ONLY_NAMED)
 }
 
 pub fn only_structs(input: &DeriveInput) -> TokenStream {
-    return Error::new_spanned(&input.ident, ONLY_STRUCTS)
-        .to_compile_error()
-        .into();
+    new_spanned_error(input, ONLY_STRUCTS)
 }
 
 pub fn no_fields(input: &DeriveInput) -> TokenStream {
-    return Error::new_spanned(&input.ident, NO_FIELDS)
-        .to_compile_error()
-        .into();
+    new_spanned_error(input, NO_FIELDS)
 }
 
 pub fn too_many_fields(input: &DeriveInput, item_count: usize) -> TokenStream {
-    return Error::new_spanned(&input.ident, format!("{TOO_MANY_FIELDS} (found {})", item_count))
-        .to_compile_error()
-        .into();
+    let message = format!("{TOO_MANY_FIELDS} (found {})", item_count);
+    new_spanned_error(input, &message)
+}
+
+pub fn parse_error(input: &DeriveInput) -> TokenStream {
+    new_spanned_error(input, PARSE_ERROR)
 }
